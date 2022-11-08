202abfb111c5888861a1f6b88ed10677ffb9a4d7
xenex0 <tarjafan09@gmail.com>
2022-11-08 20:00:18 +0200

    1

--- names/ViewController.swift
+++ names/ViewController.swift
@@ -25,8 +25,9 @@ class ViewController: UIViewController, ViewControllerDelegate {
         tableView.delegate = self
         tableView.dataSource = self
         
-        if let name = userDefaults.object(forKey: nameKey){
-            self.array[indexPath.startIndex] = name as! String
+        if let name = userDefaults.object(forKey: nameKey) as? [String] {
+            self.array = name
+           
         }
     }
     func addTableDelegate(addForViewController: String) {
@@ -38,9 +39,8 @@ class ViewController: UIViewController, ViewControllerDelegate {
         let alert =  UIAlertController(title: "select", message: "enter some name", preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "save", style: .default) { [self] _ in
             let alertText = alert.textFields?.first?.text
-            array[indexPath.row] = alertText ?? "0"
-            self.indexPath = indexPath
-            userDefaults.set(self.array[self.indexPath.startIndex], forKey: nameKey)
+            array.append(alertText ?? "")
+            userDefaults.set(self.array, forKey: nameKey)
             tableView.reloadData()
         }
         alert.addTextField()
@@ -60,28 +60,12 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: identifire, sender: self)
         
-        if tableView.isExclusiveTouch == false {
-            let alert = UIAlertController(title: "alert", message: "select person",preferredStyle: .alert)
-            let alertCancel = UIAlertAction(title: "Cancel", style: .cancel)
-            let alertAction = UIAlertAction(title: "Done", style: .default) { [self]  _ in
-                let secondView = UIStoryboard(name: "Main", bundle: nil)
-                let storyBoard = secondView.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
-                storyBoard.someText = array[indexPath.row]
-                storyBoard.delegate = self
-                self.show(storyBoard, sender: nil)
-                self.indexPath = indexPath
-            }
-            alert.addAction(alertCancel)
-            alert.addAction(alertAction)
-            alert.addTextField() { (textField) in
-                self.array[indexPath.row] = textField.text ?? ""
-            }
-            present(alert, animated: true)
-        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowDeteails" {
+        if segue.identifier ==  "MyCell" {
             let addNames = segue.destination as! SecondViewController
             addNames.delegate = self
         }
